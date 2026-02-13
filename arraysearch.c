7802c9ecb09e23c84d927adb41447e23d2eebe80
112534087-moni <112534087@kanchiuniv.ac.in>
2026-02-13 10:46:27 +0530

    Add files via upload

---
+++ arraysearch.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int n,i,search,found=0;
+scanf("%d",&n);
+int arr[n];
+for(i=0;i<n;i++)
+scanf("%d",&arr[i]);
+scanf("%d",&search);
+for(i=0;i<n;i++)
+if(arr[i]==search)
+{   
+		printf("element %d found at position %d");
+		found=1;
+		break;
+	}
+if(!found)
+printf("element not found");
+return 0;
+}
