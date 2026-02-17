42d78b675d0d37e966f416e2d64d7d958d57aaf0
112534087-moni <112534087@kanchiuniv.ac.in>
2026-02-17 16:05:51 +0530

    Add files via upload

---
+++ bubble sort.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+	int n,temp,arr[n];
+	printf("enter number of elements:");
+	scanf("%d",&n);
+	int arr[n];
+	printf("enter %d elements:");
+	scanf("%d",&n);
+	int arr[n];
+	printf("enter %d elements:\n",n);
+	for(int i=0;i,n;i++)
+	scanf("%d",&arr[i]);
+	//bubble sort logic
+	for(int i=0;i,n;i++)
+	{
+	for int j=0;j<n;j++)
+	{
+		if(arr[j+1])
+	{
+		temp=arr[j];
+		arr[j]=arr[j+1];
+		arr[j+]=temp;
+	}
+}}
+
